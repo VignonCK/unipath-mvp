a0e287f756505b65d1f915e186dc822c8e59cab3
Mr-Z1836 <harrydedji@gmail.com>
2026-04-25 13:11:03 +0100

    fix: correction de la méthode nodemailer createTransport
    - Changé createTransporter vers createTransport (méthode correcte)
    - Le serveur démarre maintenant sans erreur
    - Service email fonctionnel

--- unipath-api/src/services/email.service.js
+++ unipath-api/src/services/email.service.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   secure: false,
